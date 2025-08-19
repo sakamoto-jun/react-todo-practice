66ca0e7ae381b31369ad85e97ac0dc4d932a6e7b
sakamoto-jun <sakamotojun.dev@gmail.com>
2025-08-19 22:33:38 +0900

    동적 라우팅 적용

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,15 +1,15 @@
+import { useParams } from "react-router-dom";
 import { Todo } from "../types";
 import useStore from "../useStore";
 import Checkbox from "./Checkbox";
 
-interface TodoListProps {
-  filter?: "all" | "active" | "completed";
-}
-
-const TodoList = ({ filter }: TodoListProps) => {
+const TodoList = () => {
   const { todos, toggleTodo } = useStore();
+  const { filterType = "all" } = useParams<{
+    filterType?: "all" | "active" | "completed";
+  }>();
 
-  const filteredTodos = filterTodos(todos, filter);
+  const filteredTodos = filterTodos(todos, filterType);
 
   return (
     <ul>
@@ -27,13 +27,12 @@ const TodoList = ({ filter }: TodoListProps) => {
   );
 };
 
-function filterTodos(todos: Todo[], filter: TodoListProps["filter"]) {
+function filterTodos(todos: Todo[], filter: "all" | "active" | "completed") {
   if (filter === "active") {
     return todos.filter((todo) => !todo.done);
   } else if (filter === "completed") {
     return todos.filter((todo) => todo.done);
   }
-
   return todos;
 }
 
